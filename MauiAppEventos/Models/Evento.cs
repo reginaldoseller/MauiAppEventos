881ccd34357aeb411164b733c0f479649a72943d
Reginaldo Séller <acordeseller@gmail.com>
2026-05-28 17:00:43 -0300

    Primeiro commit limpando a pasta VS

---
+++ MauiAppEventos/Models/Evento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppEventos.Models
+{
+    public class Evento
+    {
+        public string NomeDoEvento { get; set; }
+        public string LocalDoEvento { get; set; }
+        public int NroParticipantes { get; set; }
+        public double CustoPorParticipante { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public int QuantDias
+        {
+            get
+            {
+                TimeSpan intervalo = DataFim - DataInicio;
+                return intervalo.Days;
+            }
+        }
+        
+        public double VlrTotal
+        {
+            get
+            {
+                double tot = NroParticipantes * CustoPorParticipante * QuantDias;
+                return tot;
+            }
+        }
+    }
+}
---
+++ MauiAppEventos/Views/RelatorioEvento.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppEventos.Views;
+
+public partial class RelatorioEvento : ContentPage
+{
+	public RelatorioEvento()
+	{
+		InitializeComponent();
+	}
+}
